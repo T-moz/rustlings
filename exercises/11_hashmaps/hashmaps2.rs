41fc41b0c7378af00e450fa4d49b03dec8c7a11a
Matthieu Pernelle <matthieu.pernelle@gmail.com>
2024-02-25 14:14:07 +0100

    :bug: (11 hashmaps)

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -37,9 +37,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        if basket.get(&fruit).copied().unwrap_or(0) > 0 {
+        } else {
+            basket.insert(fruit, 3);
+        }
     }
 }
 
@@ -81,7 +82,7 @@ mod tests {
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
-    
+
     #[test]
     fn all_fruit_types_in_basket() {
         let mut basket = get_fruit_basket();
